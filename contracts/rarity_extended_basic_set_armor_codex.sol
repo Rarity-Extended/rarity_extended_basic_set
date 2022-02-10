012a9b66e246a1e4eed65b4960d066bb2535cc7a
TheAustrian1998 <theaustrianeth@gmail.com>
2022-02-10 11:34:55 -0300

    Add some new items

--- contracts/rarity_extended_basic_set_armor_codex.sol
+++ contracts/rarity_extended_basic_set_armor_codex.sol
@@ -37,6 +37,18 @@ contract basic_set_armors_codex {
             return slain_warrior_armor();
         } else if (_id == 4) {
             return random_shield();
+        }else if (_id == 8) {
+            return sorcerer_hat();
+        }else if (_id == 9) {
+            return warrior_helmet();
+        }else if (_id == 10) {
+            return hero_boots();
+        }else if (_id == 11) {
+            return leather_shoes();
+        }else if (_id == 12) {
+            return metal_glove();
+        }else if (_id == 13) {
+            return glove_with_spikes();
         }
     }
 
@@ -184,6 +196,54 @@ contract basic_set_armors_codex {
         description = "Every warrior needs one";
     }
 
+    function hero_boots() public pure returns (
+        uint id,
+        uint cost,
+        uint proficiency,
+        uint weight,
+        uint armor_bonus,
+        uint max_dex_bonus,
+        int penalty,
+        uint spell_failure,
+        string memory name,
+        string memory description
+    ) {
+        id = 11;
+        name = "Hero boots";
+        cost = 15e18;
+        proficiency = 1;
+        weight = 3;
+        armor_bonus = 2;
+        max_dex_bonus = 2;
+        penalty = 0;
+        spell_failure = 10;
+        description = "Keeps your feet warm";
+    }
+
+    function leather_shoes() public pure returns (
+        uint id,
+        uint cost,
+        uint proficiency,
+        uint weight,
+        uint armor_bonus,
+        uint max_dex_bonus,
+        int penalty,
+        uint spell_failure,
+        string memory name,
+        string memory description
+    ) {
+        id = 11;
+        name = "Leather shoes";
+        cost = 15e18;
+        proficiency = 1;
+        weight = 3;
+        armor_bonus = 2;
+        max_dex_bonus = 2;
+        penalty = 0;
+        spell_failure = 10;
+        description = "How many boars had been killed for making this shoes?";
+    }
+
     function metal_glove() public pure returns (
         uint id,
         uint cost,
@@ -208,7 +268,7 @@ contract basic_set_armors_codex {
         description = "Perfect to hit, uncomfy to wash your teeth";
     }
 
-    function hero_boots() public pure returns (
+    function glove_with_spikes() public pure returns (
         uint id,
         uint cost,
         uint proficiency,
@@ -220,15 +280,16 @@ contract basic_set_armors_codex {
         string memory name,
         string memory description
     ) {
-        id = 11;
-        name = "Hero boots";
-        cost = 15e18;
+        id = 10;
+        name = "Glove with spikes";
+        cost = 10e18;
         proficiency = 1;
-        weight = 3;
-        armor_bonus = 2;
+        weight = 2;
+        armor_bonus = 1;
         max_dex_bonus = 2;
         penalty = 0;
         spell_failure = 10;
-        description = "Keeps your feet warm";
+        description = "A tool to shake the enemy's hand";
     }
+
 }
